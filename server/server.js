66e348f20fb1f5d38bbba0656e98fdfb17874e8e
Sumanth Varma Vitrouthu <vitrouthu@gmail.com>
2021-03-20 22:23:17 -0700

    First working prototype

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
 const db = mysql.createConnection({
   user: "root",
